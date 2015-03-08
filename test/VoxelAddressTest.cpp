b583a8afacbc1ed129f22dedf6081568a99acd68
Leo Dearden <leo@leodearden.com>
2015-03-08 17:39:50 +0000

    Make indenting consistent.

--- test/VoxelAddressTest.cpp
+++ test/VoxelAddressTest.cpp
@@ -20,18 +20,18 @@ namespace {
         virtual void TearDown() {}
     };
 
-TEST_F(VoxelAddressTest, Construction) {
+    TEST_F(VoxelAddressTest, Construction) {
         VoxelAddress(1, 2, 3);
     }
 
     // This test demonstrates why we don't make address bit masks by right shifting.
-TEST_F(VoxelAddressTest, bitwiseRightShiftFlowsZerosFromTop) {
+    TEST_F(VoxelAddressTest, bitwiseRightShiftFlowsZerosFromTop) {
         VoxelAddress::addr_t topSet = 0x8000000000000000;
         VoxelAddress::addr_t topFourSet = 0xf000000000000000;
         ASSERT_NE(topFourSet, (topSet >> 3));
     }
 
-TEST_F(VoxelAddressTest, thatGetSubtreeIndexReturnsZeroForAZeroAddress) {
+    TEST_F(VoxelAddressTest, thatGetSubtreeIndexReturnsZeroForAZeroAddress) {
         VoxelAddress allZero(0, 0, 0);
         ASSERT_EQ(0, allZero.getSubtreeIndex(0));
     }
