8aa703692f41dd63c2b5aaafde35981960898262
Leo Dearden <leo@leodearden.com>
2015-03-08 16:05:50 +0000

    Add Octree class (stub).

---
+++ src/include/Octree.hpp
@@ -0,0 +1,31 @@
+#ifndef OCTREE_HPP
+#define OCTREE_HPP
+
+#include <boost/shared_ptr.hpp>
+#include <bitset>
+
+namespace octvox {
+
+    class VoxelAddress;
+
+    class Octree {
+    public:
+        Octree(const std::bitset<volume> &_voxels, const VoxelAddress);
+
+        ~Octree();
+
+        Octree(const Octree &) = default;
+
+        bool getVoxel(const VoxelAddress) const;
+
+        boost::shared_ptr<const Octree> intersectionWith(boost::shared_ptr<const Octree> other) const;
+
+        boost::shared_ptr<const Octree> unionWith(boost::shared_ptr<const Octree> other) const;
+
+        bool operator==(const Octree &other) const;
+
+    private:
+    };
+
+}
+#endif
---
+++ test/OctreeTest.cpp
@@ -0,0 +1,82 @@
+#include "gtest/gtest.h"
+#include "Octree.hpp"
+#include "VoxelAddress.hpp"
+#include <boost/make_shared.hpp>
+
+namespace {
+
+    using boost::shared_ptr;
+    using boost::make_shared;
+    using namespace octvox;
+
+    class OctreeTest : public ::testing::Test {
+    protected:
+        shared_ptr<const Octree> emptyTree;
+        shared_ptr<const Octree> fullTree;
+        shared_ptr<const Octree> partialTree;
+        shared_ptr<const Octree> a;
+        shared_ptr<const Octree> a2;
+        shared_ptr<const Octree> b;
+        shared_ptr<const Octree> aIntersectB;
+        shared_ptr<const Octree> aUnionB;
+
+        OctreeTest() {
+
+        }
+
+        virtual ~OctreeTest() {}
+
+        virtual void SetUp() {}
+
+        virtual void TearDown() {}
+    };
+
+    TEST_F(OctreeTest, Construction) {}
+
+#if 0
+    TEST_F(OctreeTest, getVoxelReturnsFalseWhenGettingAnEmptyVoxel) {
+        ASSERT_FALSE(emptyTree->getVoxel(origin));
+    }
+
+    TEST_F(OctreeTest, getVoxelReturnsTrueWhenGettingAFullVoxel) {
+        ASSERT_TRUE(fullTree->getVoxel(origin));
+    }
+
+    TEST_F(OctreeTest, getVoxelReturnsCorrectlyAtNonZeroAddress) {
+        ASSERT_TRUE(partialTree->getVoxel(full));
+        ASSERT_FALSE(partialTree->getVoxel(empty));
+    }
+
+    TEST_F(OctreeTest, getVoxelAliasesOutOfRangeAddresses) {
+        VoxelAddress outOfRangeFull(Octree::edgeLength + full.x, Octree::edgeLength + full.y, Octree::edgeLength + full.z);
+        VoxelAddress outOfRangeEmpty(Octree::edgeLength + empty.x, Octree::edgeLength + empty.y, Octree::edgeLength + empty.z);
+
+        ASSERT_TRUE(partialTree->getVoxel(outOfRangeFull));
+        ASSERT_FALSE(partialTree->getVoxel(outOfRangeEmpty));
+    }
+
+    TEST_F(OctreeTest, equalReturnsTrueOnIdenticalOctrees) {
+        ASSERT_TRUE(*a == *a);
+    }
+
+    TEST_F(OctreeTest, equalReturnsFalseOnUnequalOctrees) {
+        ASSERT_FALSE(*a == *b);
+    }
+
+    TEST_F(OctreeTest, equalReturnsTrueOnEqualButNotIdenticalOctrees) {
+        ASSERT_TRUE(*a == *a2);
+    }
+
+    TEST_F(OctreeTest, intersectionWorks) {
+        shared_ptr<const Octree> i = a->intersectionWith(b);
+        ASSERT_TRUE(*i == *aIntersectB);
+    }
+
+    TEST_F(OctreeTest, unionWorks) {
+        shared_ptr<const Octree> i = a->unionWith(b);
+        ASSERT_TRUE(*i == *aUnionB);
+    }
+
+    #endif
+
+}  // namespace
