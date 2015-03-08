c85ccd2c5ec72bab5b40873b04defe1f5d89681d
Leo Dearden <leo@leodearden.com>
2015-03-08 21:34:34 +0000

    Create Specialisation Octree<0> for nodes that have OctLeaf children.

--- src/include/Octree.hpp
+++ src/include/Octree.hpp
@@ -5,20 +5,60 @@
 #include "VoxelAddress.hpp"
 
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <bitset>
 
 namespace octvox {
 
     class VoxelAddress;
 
+    class OctreeHoist {
+    public:
+
+    };
+
     template<uint_fast8_t height>
-    class Octree {
+    class Octree : public OctreeHoist {
+    public:
+        static const size_t childrenSize = 8;
+
+        // For testing.
+        Octree(const std::bitset<childrenSize> _full) : full(_full) {}
+
+        // For testing.
+        bool getVoxel(const VoxelAddress addr) const {
+            auto childIndex = addr.getSubtreeIndex(height);
+            if(full[childIndex]) {
+                return true;
+            } else if (children[childIndex].use_count() == 0) {
+                return false;
+            } else {
+                return children[childIndex]->getVoxel(addr);
+            }
+        }
+
+        boost::shared_ptr<const Octree> setLeaf(const OctLeaf& leaf, const VoxelAddress) const {
+            return boost::make_shared<const Octree>();
+        }
+
+        boost::shared_ptr<const Octree> intersectionWith(boost::shared_ptr<const Octree> other) const;
+        boost::shared_ptr<const Octree> unionWith(boost::shared_ptr<const Octree> other) const;
+
+        bool operator==(const Octree &other) const;
+
+    private:
+        std::bitset<childrenSize> full;
+        std::array<boost::shared_ptr<const Octree>, childrenSize> children;
+    };
+
+    template<>
+    class Octree<0> {
     public:
         static const size_t childrenSize = 8;
 
         Octree() = default;
         // For testing.
-        Octree(std::bitset<childrenSize> _full) : full(_full) {}
+        Octree(const std::bitset<childrenSize> _full) : full(_full) {}
 
         ~Octree() {}
 
@@ -36,8 +76,8 @@ namespace octvox {
             }
         }
 
-        boost::shared_ptr<const Octree> setLeaf(const OctLeaf&& leaf, const VoxelAddress) {
-            return boost::shared_ptr<Octree const>();
+        boost::shared_ptr<const Octree> setLeaf(const OctLeaf& leaf, const VoxelAddress) const {
+            return boost::make_shared<const Octree>();
         }
 
         boost::shared_ptr<const Octree> intersectionWith(boost::shared_ptr<const Octree> other) const;
@@ -46,9 +86,9 @@ namespace octvox {
         bool operator==(const Octree &other) const;
 
     private:
-        std::bitset<childrenSize> populated;
+        static const uint_fast8_t height = 0;
         std::bitset<childrenSize> full;
-        std::array<boost::shared_ptr<const Octree>, childrenSize> children;
+        std::array<boost::shared_ptr<const OctLeaf>, childrenSize> children;
     };
 
 
