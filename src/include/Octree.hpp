55c663a224126f215ab0178e3c845040088bcd40
Cass May <cass@cassm.net>
2015-04-10 23:33:57 +0100

    Move Octree children and fullness into Child subclass

--- src/include/Octree.hpp
+++ src/include/Octree.hpp
@@ -27,24 +27,23 @@ namespace octvox {
 
         enum class Fullness {empty, partial, full};
 
-        //using childrenType = std::array<childHolder, childrenSize>;
-        class Children {
+        // TODO move this out of Octree
+        class Child {
         public:
-            Children() noexcept {fullness.fill(Fullness::empty);}
-            inline void fill(subtreeIndexType) noexcept;
-            inline void empty(subtreeIndexType) noexcept;
-            inline void set(subtreeIndexType, std::shared_ptr<const childType> child);
+            Fullness fullness;
+            std::shared_ptr<const childType> child;
+            Child() : fullness(Fullness::empty) {}
+            inline void fill() noexcept;
+            inline void empty() noexcept;
+            inline void set(std::shared_ptr<const childType> child);
             inline bool get(const VoxelAddress addr) const noexcept;
-            inline bool operator==(const Octree<height>::Children& other) const noexcept;
-            inline bool operator!=(const Octree<height>::Children& other) const noexcept;
-        private:
-            std::array<Fullness, childrenSize> fullness;
-            std::array<std::shared_ptr<const childType>, childrenSize> children;
+            inline bool operator==(const Octree<height>::Child& other) const noexcept;
+            inline bool operator!=(const Octree<height>::Child& other) const noexcept;
         };
 
-        Octree() noexcept = default;
+        Octree() = default;
         // ToDo: convert parameter type to rvalue ref and move
-        Octree(Children _children) noexcept : children(_children) {}
+        Octree(std::array<Child, childrenSize> _children) noexcept : children(_children) {}
         // For Testing. Sets the OctLeaf that contains addr to leaf.
         Octree(const std::shared_ptr<const OctLeaf> leaf, const VoxelAddress addr) noexcept;
 
@@ -58,7 +57,7 @@ namespace octvox {
         bool getVoxel(const VoxelAddress addr) const;
 
     private:
-        Children children;
+        std::array<Child, childrenSize> children;
     };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,12 +65,12 @@ namespace octvox {
 
     template <heightType height>
     Octree<height>::Octree(const std::shared_ptr<const OctLeaf> leaf, const VoxelAddress addr) noexcept {
-        children.set(addr.getSubtreeIndex(height), std::make_shared<const Octree<height - 1> >(leaf, addr));
+        children[addr.getSubtreeIndex(height)].set(std::make_shared<const Octree<height - 1> >(leaf, addr));
     }
 
     template<>
     Octree<0>::Octree(const std::shared_ptr<const OctLeaf> leaf, const VoxelAddress addr) noexcept {
-        children.set(addr.getSubtreeIndex(0), leaf);
+        children[addr.getSubtreeIndex(0)].set(leaf);
     }
 
     template <heightType height>
@@ -87,58 +86,67 @@ namespace octvox {
     template<heightType height>
     inline std::shared_ptr<const Octree<height> >Octree<height>::intersectionWith(
             std::shared_ptr<const octvox::Octree<height> > other) const {
-        Children newChildren;
+        std::array<Child, childrenSize> newChildren;
         for(int i = 0; i < childrenSize; ++i) {
-            //newChildren[i] = children[i]->intersectionWith(other->children[i]);
+            if (children[i].fullness == Fullness::empty || other->children[i].fullness == Fullness::empty) {
+                // newly constructed child defaults to empty
+                //newChildren[i].fullness = Fullness::empty;
+            }
+            else if (children[i].fullness == Fullness::full && other->children[i].fullness == Fullness::full) {
+                newChildren[i].fullness = Fullness::full;
+            }
+            else {
+                newChildren[i].fullness = Fullness::partial;
+                newChildren[i].child = children[i].child->intersectionWith(other->children[i].child);
+            }
         }
         return std::make_shared<const Octree<height> >(newChildren);
     }
 
     template<heightType height>
     bool Octree<height>::getVoxel(const VoxelAddress addr) const {
-        return children.get(addr);
+        return children[addr.getSubtreeIndex(height)].get(addr);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Octree::Children Public Methods
+    // Octree::Child Public Methods
 
     template <heightType height>
-    inline void Octree<height>::Children::fill(subtreeIndexType i) noexcept {
-        fullness[i] = Fullness::full;
-        children[i].reset();
+    inline void Octree<height>::Child::fill() noexcept {
+        fullness = Fullness::full;
+        child.reset();
     }
 
     template <heightType height>
-    inline void Octree<height>::Children::empty(subtreeIndexType i) noexcept {
-        fullness[i] = Fullness::empty;
-        children[i].reset();
+    inline void Octree<height>::Child::empty() noexcept {
+        fullness = Fullness::empty;
+        child.reset();
     }
 
     template <heightType height>
-    inline void Octree<height>::Children::set(subtreeIndexType i, std::shared_ptr<const childType> child) {
-        fullness[i] = Fullness::partial;
-        children[i] = child;
+    inline void Octree<height>::Child::set(std::shared_ptr<const childType> _child) {
+        fullness = Fullness::partial;
+        child = _child;
     }
 
     template <heightType height>
-    inline bool Octree<height>::Children::get(VoxelAddress addr) const noexcept {
-        auto childIndex = addr.getSubtreeIndex(height);
-        if (fullness[childIndex] == Fullness::empty) {
+    inline bool Octree<height>::Child::get(VoxelAddress addr) const noexcept {
+        if (fullness == Fullness::empty) {
             return false;
-        } else if (fullness[childIndex] == Fullness::full){
+        } else if (fullness == Fullness::full){
             return true;
         } else {
-            return children[childIndex]->getVoxel(addr);
+            return child->getVoxel(addr);
         }
     }
 
     template <heightType height>
-    inline bool Octree<height>::Children::operator==(const Octree<height>::Children& other) const noexcept {
+    inline bool Octree<height>::Child::operator==(const Octree<height>::Child& other) const noexcept {
         if(fullness != other.fullness) {
             return false;
         } else {
             for(int i = 0; i < childrenSize; ++i) {
-                if (fullness[i] == Fullness::partial && (*children[i] != *(other.children[i]))) {
+                if (fullness == Fullness::partial && (*child != *(other.child))) {
                     return false;
                 }
             }
@@ -147,7 +155,7 @@ namespace octvox {
     }
 
     template <heightType height>
-    inline bool Octree<height>::Children::operator!=(const Octree<height>::Children& other) const noexcept {
+    inline bool Octree<height>::Child::operator!=(const Octree<height>::Child& other) const noexcept {
         return !operator==(other);
     }
 
